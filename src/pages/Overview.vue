a3d8433e5704749c02a6dbef40d24f36cae94051
Peter Bösenberg <peterboesenerg@gmail.com>
2018-12-01 22:38:03 +0100

    quadrants base layout

---
+++ src/pages/Overview.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page>
+    <div class="quadrant">tasks</div>
+    <div class="quadrant">okr</div>
+    <div class="quadrant">backlog</div>
+    <div class="quadrant">health</div>
+  </q-page>
+</template>
+
+<style>
+.quadrant{
+  width: 50%;
+  height: 50vh;
+  display: inline-block;
+}
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
